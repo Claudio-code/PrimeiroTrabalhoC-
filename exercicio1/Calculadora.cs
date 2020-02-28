90fe2d6a472a5197668c1e0e73b93fdce4e996db
claudio-code <suporte2424@outlook.com>
2020-02-28 20:09:51 -0300

    criada a classe da calculadora

---
+++ exercicio1/Calculadora.cs
@@ -0,0 +1,22 @@
+namespace exercicio1
+{
+    public class Calculadora
+    {
+        public decimal soma(decimal valor1, decimal valor2) {
+
+            return valor1 + valor2;
+        } 
+
+        public decimal subtracao(decimal valor1, decimal valor2) {
+            return valor1 - valor2;
+        }
+
+        public decimal divisao(decimal valor1, decimal valor2) {
+            return valor1 - valor2;
+        }
+
+        public decimal multiplicacao(decimal valor1, decimal valor2) {
+            return valor1 * valor2;
+        }
+    }
+}
